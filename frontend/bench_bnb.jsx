b1d42d8517035a96e9fc801d3914e3f4b5eeeea0
Josh Turgeon <j_turgeon88@yahoo.com>
2018-04-06 15:29:28 -0700

    Part 1 phase 1 - SessionApiUtil created and tested

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TESTING PURPOSES:
+import * as APIUtil from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
+  // TESTING PURPOSES ->
+  window.signup = APIUtil.signup;
+  window.login = APIUtil.login;
+  window.logout = APIUtil.logout;
+  // <- TESTING PURPOSES
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB sucka</h1>, root);
 });
