0b78cd5f95f9261b3efe93b46fddc005085f1c23
Josh Turgeon <j_turgeon88@yahoo.com>
2018-04-06 14:01:58 -0700

    Part 1 phase 1 - Auth backend complete and tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil       #Does this return an empty object {}?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def ensure_logged_in
+
+  end
 end
