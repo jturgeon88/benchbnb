04b47d0da5b0d3ebadaaf2bc288cf687116f6dbc
Josh Turgeon <j_turgeon88@yahoo.com>
2018-04-06 21:32:38 -0700

    Phase 1 complete and tested

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/store';
 // TESTING PURPOSES:
 import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // TESTING PURPOSES ->
-  window.signup = APIUtil.signup;
-  window.login = APIUtil.login;
-  window.logout = APIUtil.logout;
-  // <- TESTING PURPOSES
   const root = document.getElementById('root');
+  const store = configureStore();
+  // TESTING PURPOSES ->
+  // window.signup = APIUtil.signup;
+  // window.login = APIUtil.login;
+  // window.logout = APIUtil.logout;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // <- TESTING PURPOSES
   ReactDOM.render(<h1>Welcome to BenchBnB sucka</h1>, root);
 });
