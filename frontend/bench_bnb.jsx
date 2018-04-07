276bea99d7f88929ae7e40502929cceacba16437
Josh Turgeon <j_turgeon88@yahoo.com>
2018-04-06 22:21:29 -0700

    Phase 2 begun, App and Root components added, entry file updated

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-// TESTING PURPOSES:
-import * as APIUtil from './util/session_api_util';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
   const store = configureStore();
+  
   // TESTING PURPOSES ->
   // window.signup = APIUtil.signup;
   // window.login = APIUtil.login;
@@ -14,5 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   // <- TESTING PURPOSES
-  ReactDOM.render(<h1>Welcome to BenchBnB sucka</h1>, root);
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store}/>, root);
 });
