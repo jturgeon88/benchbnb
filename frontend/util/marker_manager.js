fdc6c413b64376cfd249364c47c1a971578dbe9c
Josh Turgeon <j_turgeon88@yahoo.com>
2018-04-16 15:01:32 -0700

    Phase 8 complete and tested

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -6,7 +6,22 @@ export default class MarkerManager {
     this.markers = {};
   }
 
+  createMarkerFromBench(bench) {
+    const position = new google.maps.LatLng(bench.lat, bench.lng);
+    const marker = new google.maps.Marker({
+      position: position,
+      map: this.map,
+      benchId: bench.id
+    });
+
+    this.markers[marker.benchId] = marker;
+  }
+
   updateMarkers(benches) {
+    benches.forEach(newBench => (
+      this.createMarkerFromBench(newBench)
+    ));
     console.log('time to update');
   }
+
 }
